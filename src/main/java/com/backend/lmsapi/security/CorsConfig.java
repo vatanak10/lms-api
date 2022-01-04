e43e5646b5ed86088e9c641007453d4419a04219
vatanak10 <koeurnsereyvatanak@gmail.com>
2022-01-04 14:10:06 +0700

    :lock: Enable CORS Configuration

---
+++ src/main/java/com/backend/lmsapi/security/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.backend.lmsapi.security;
+
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+
+        config.setAllowedOrigins(Arrays.asList("http://localhost:8081", "http://localhost:4200"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
+        config.setExposedHeaders(Collections.singletonList("x-auth-token"));
+        FilterRegistrationBean<?> bean = new FilterRegistrationBean(new CorsFilter(source));
+        source.registerCorsConfiguration("/**", config);
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+}
